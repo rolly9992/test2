8cfc130b07b989f539dc4dd3650bf7322cad96a2
rolly9992 <robertomara101@comcast.net>
2024-12-14 10:05:38 -0800

    added a new test py file printing loop values

---
+++ testing_print_loop_values.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 15:46:24 2020
+
+@author: Robert
+"""
+
+
+import numpy as np 
+
+print(list(range(1,5)))
+
+l = list(range(1,5))
+for i in (range(len(l))):
+    print(i)
+#    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
