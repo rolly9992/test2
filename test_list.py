a7e006003cae78ebccd25e3a084680f6c7a65782
rolly9992 <robertomara101@comcast.net>
2024-12-14 07:47:31 -0800

    test load from this computer via git

---
+++ test_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 18:49:11 2022
+
+@author: Python
+"""
+
+
+
+l=[1,0,1,1,0]
+print(sum(l))
+
+
+import random 
+
+s = range(20)
+
+r=random.sample(s,20)
+
+print(r)
+
